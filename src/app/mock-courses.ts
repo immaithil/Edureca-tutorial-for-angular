3fd90adc79424a30f8a5649f94bf29b169d19f19
Ashish Anand <ashishanand@Halodocs-MacBook-Pro-3.local>
2021-11-22 14:32:03 +0530

    first commit

---
+++ src/app/mock-courses.ts
@@ -0,0 +1,10 @@
+import { Course } from "./course";
+
+export const COURSES: Course[]=[
+    {id:1, name:"Java"},
+    {id:2, name:"HTML"},
+    {id:3, name:"Javascript"},
+    {id:4, name:"CSS"},
+    {id:5, name:"React"},
+    {id:6, name:"Autocad"}
+]
